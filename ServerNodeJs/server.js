4cec4c7042e689b21be6a6795e4439a1ebd890ec
Irene Koech <rynchero@student.lnu.se>
2020-04-16 15:06:11 +0200

    Updated on port

--- ServerNodeJs/server.js
+++ ServerNodeJs/server.js
@@ -3,6 +3,7 @@ const app = express()
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+var PORT = 3000
 require('dotenv/config')
 
 //Middlewares
@@ -14,12 +15,16 @@ app.use(express.json())*/
 //Import Routes
 const postsRoute = require('./routes/posts')
 app.use('/posts', postsRoute)
-//Routes
 app.get('/', (req, res) =>{
-    res.send('Server is running')
+    res.send('Home Page')
 })
+
 //Connect to database
 mongoose.connect(process.env.DB_CONNECTION,
 { useNewUrlParser: true, useUnifiedTopology: true },
  () => console.log('connected to DB!'))
-app.listen(3000)
+
+ //Listen to Port 3000
+ app.listen(PORT, () =>{
+     console.log(`Listening on port ${PORT}`)
+ })
